4e4ed3ec3e54e0a4e631e75bad101b4f3e51befb
Schmid Jonas <Jonas.Schmid@calida.com>
2016-12-15 13:42:11 +0100

    Wochenplan verbessert datum und text sowie "" hinzugefügt

--- source/function.php
+++ source/function.php
@@ -12,16 +12,22 @@ if (!file_exists($database))
 $guestbookEntries = file_get_contents($database);
 $errors = [];
 
+//bestimmt das datum
+$timestamp = time();
+$datum = date("d.m.Y",$timestamp);
+$uhrzeit = date("H:i",$timestamp);
+$date = $datum ." - " .$uhrzeit ." Uhr";
+
  if($_SERVER['REQUEST_METHOD'] === 'POST')
   {
       // prüfen ob variable vorhanden sonst''
       $name = strip_tags ($_POST['name']) ?? '';
+      $name = '"' .$name .'"';
       // prüfen ob <> eingegeben wurden
       $name = htmlentities ($name);
       $message = strip_tags ($_POST['note']) ?? '';
       $message = htmlentities ($message);
-      $newEntry = "<h2>" .$name ."</h2>" .$message ."<br>";
-      echo $newEntry;
+      $newEntry = "<h3>Dieser Beitrag wurde erstellt von " .$name ." am " .$date ."</h3>" .$message ."<br>";
 
       if ($name === '') {
         $errors[] = 'Bitte geben sie einen Namen ein.';
@@ -30,7 +36,7 @@ $errors = [];
         $errors[] = 'Bitte geben sie eine Nachricht ein';
       }
 
-      $guestbookEntries .= $newEntry;
+      $guestbookEntries = $newEntry. $guestbookEntries;
 
       if (count($errors) === 0) {
         //daten($guestbookEntries) in Datenbank speichern
@@ -44,11 +50,10 @@ $errors = [];
       }
 
       if (count($errors > 0)) {
+        echo ("<br>");
         foreach ($errors as $value)
         echo ("<ul><li>$value</li></ul>");
       }
-
-
 
       // index.php wird aufgerufen dann nicht mehr POST
       //header ("Location: index.php");
